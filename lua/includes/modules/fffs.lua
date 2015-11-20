30e4176b15ddf43d6a0fa4595073adb5c46cdc5b
Swad <SwadicalRag@users.noreply.github.com>
2015-11-20 22:53:41 +1100

    Basic filesystem manipulation functions

---
+++ lua/includes/modules/fffs.lua
@@ -0,0 +1,41 @@
+local FFFS = {}
+
+FFFS.BaseFS = {}
+do
+	local FS = FFFS.BaseFS
+
+	FS.data = {}
+	FS.currentDir = FS.data
+	FS.path = "/"
+
+	function FS:CreateDir(name)
+		self.currentDir[name] = {
+			[".."] = self.currentDir
+		}
+	end
+
+	function FS:Delete(name)
+		self.currentDir[name] = nil
+	end
+
+	function FS:CreateFile(name,data)
+		if(type(self.currentDir[name]) == "table") then
+			error("Attempt to create a file with the same identifier as a folder")
+		else
+			self.currentDir[name] = data or ""
+		end
+	end
+
+	function FS:ChangeDir(name)
+		if(type(self.currentDir[name]) == "table") then
+			self.currentDir = self.currentDir[name]
+			self.path = self.path..name.."/"
+		else
+			error(self.path..name.." is not a directory!")
+		end
+	end
+
+	function FS:Search(name)
+		-- to-do
+	end
+end
